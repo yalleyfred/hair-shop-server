e18c5c2b09e9152040f6f89702d4a01ad7f3f7e4
yalleyfred <yalleyfred@gmail.com>
2026-02-07 21:10:40 +0000

    refactor: use flyway for db migrations

--- db/migration/V1__prod_add_product_quantity.sql
+++ db/migration/V1__prod_add_product_quantity.sql
@@ -1,2 +1,7 @@
+-- up
 ALTER TABLE "products"
 ADD COLUMN "quantity" integer NOT NULL DEFAULT 1;
+
+-- down
+ALTER TABLE "products"
+DROP COLUMN "quantity";
--- src/migrations/V2__prod_add_product_quantity copy.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "products"
-ADD COLUMN "quantity" integer NOT NULL DEFAULT 1;
